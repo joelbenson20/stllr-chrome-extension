cdbc888061ac8eff2c8b396ea6350640a1ca190e
Joel Benson <joelbenson20@gmail.com>
2026-05-02 20:28:23 -0500

    more comments js updates

--- comments.js
+++ comments.js
@@ -28,7 +28,10 @@ function initFormSubmission(form) {
 
                 // Initialize new reply form
                 var newComment = document.querySelector(`#comment-${response.commentId}`);
-                initComment(newComment);
+                newStarButton = newComment.querySelector('.comment-star-button');
+                newForm = newComment.querySelector('.comment-form')
+                initCommentStarButton(newStarButton);
+                initCommentForm(newForm);
 
                 // Close form container for threaded comments
                 if (parentId) {
@@ -187,23 +190,21 @@ function initmarkdownEditButton(button) {
     initmarkdownEditButton(markdownEditButton);
 }
 
- function initComment(comment) {
-    initCommentStarButton(comment.querySelector('.comment-star-button'));
+ function initComments() {
 
-    var commentForm = comment.querySelector('.comment-form')
-    initCommentForm(commentForm);
- }
-
- export function initComments() {
-
-    // Initialize all comments and their forms
-    var comments = document.querySelectorAll('.comment');
-    comments.forEach(comment => {
-        initComment(comment);
+    // Initialize star buttons
+    var commentStarButtons = document.querySelectorAll('.comment-star-button');
+    commentStarButtons.forEach(button => {
+        initCommentStarButton(button);
     })
 
-    // Initialize the root (first) comment form
-    var rootForm = document.querySelector('.comment-form');
-    initCommentForm(rootForm);
+    // Initialize comment forms
+    var commentForms = document.querySelectorAll('.comment-form');
+    commentForms.forEach(form => {
+        initCommentForm(form)
+    })
 
  }
+
+
+initComments();
